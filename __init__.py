2393a37e38ba07b267b9859783f0499bd2a2fa04
Colin Rabyniuk <colin@madebyarticle.com>
2019-11-01 13:45:56 -0400

    empt init

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from flask import Flask
+# from flask import Flask
 
-# Initialize the app
-app = Flask(__name__, instance_relative_config=True)
+# # Initialize the app
+# app = Flask(__name__, instance_relative_config=True)
 
-from NotionApp import app as application
+# from NotionApp import app as application

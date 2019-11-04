37b52b4f9cc683207484b4dc51059b4f1b870e62
Colin Rabyniuk <colin@madebyarticle.com>
2019-11-04 14:10:58 -0500

    update: add todays date to last_contact field; add status to rows

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from datetime import date
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS, cross_origin
 from notion.client import NotionClient
 from notion.block import TextBlock
+from notion.collection import NotionDate
 
 import sys
 import json
@@ -11,35 +13,40 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def hello():
-    return "Hello, Flask!"
+    client = NotionClient(
+        token_v2="9a77f433e1fd89ba5ead35ecc8c1e4d446398843d213840a309f9fd51032aeab94e9da37b9fcf73699f99c5fcfb9281e3431d558cfa80cf4b070eba9fe30b3302d5d53f4f071b8096513708dee9b")
+    cv = client.get_collection_view(
+        'https://www.notion.so/madebyarticle/29fe85ffc337456bb079698ee42ef9d7?v=8cebb3b6b1f048e68c33f7e0720714e0')
+    
+    for row in cv.collection.get_rows():
+        print(row.last_contact.start)
+
 
 # @crossdomain(origin='*', headers=['access-control-allow-origin', 'Content-Type'])
 @app.route('/api/notion', methods=['POST'])
 @cross_origin()
 def create_row():
-    # raise exception(request)
     content = request.get_json()
-    # if not content.get('company'):
-    #     abort(400, 'it broke dood')
-
-
-    print(content)
-    # return jsonify({'request': 'true'}), 201
 
     company = content.get('company')
     contact = content.get('contact')
     email = content.get('email')
     desc = content.get('description')
+    newDate = NotionDate(date.today())
+    status = 'Lead'
 
     client = NotionClient(
         token_v2="9a77f433e1fd89ba5ead35ecc8c1e4d446398843d213840a309f9fd51032aeab94e9da37b9fcf73699f99c5fcfb9281e3431d558cfa80cf4b070eba9fe30b3302d5d53f4f071b8096513708dee9b")
     cv = client.get_collection_view(
-        'https://www.notion.so/madebyarticle/bb81bb0ef05547eab80b7a24339a3679?v=19f71a5d05c74810a8ebbf5f76580ca7')
-
+        'https://www.notion.so/madebyarticle/29fe85ffc337456bb079698ee42ef9d7?v=8cebb3b6b1f048e68c33f7e0720714e0')
+    
     row = cv.collection.add_row()
+    row.icon = '🐍'
     row.company = company
     row.contact = contact
     row.email = email
+    row.last_contact = newDate
+    row.status = status
     row.children.add_new(TextBlock, title=desc)
 
     return jsonify({'done': 'true'}), 201
